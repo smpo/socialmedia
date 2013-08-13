02bcd666882bfcfc095ace8ba3d9ee634f2b2e85
vu-ostracism@mail.com <VUOstracismW2012>
2013-08-13 21:51:21 +0300

    updated source

--- main.js
+++ main.js
@@ -1,46 +1,84 @@
+// **Instructions** **main.js**
+// ------------
+// In this document you will find instructions on how to adjust different parameters of the paradigm. You can apply the desired changes to the document main.js on your computer or server, using a source code editor.
+// The following parameters are necessary to adjust: number of avatar images, and the redirect link at the end of the study. All other parameters have a default option and adjustments are optional.
+
 $(function() {
 
-  // Settings
-  // Can be modified to fit the needs of the experimenter
+  // **Parameters**
+  // ------------
+  
   function set_settings() {
     window.settings = [];
-
-    settings.numberofavatars = 82; // number of avatars the user can choose from, default: 82
-    settings.tasklength = 180000; // length of the task in milliseconds, default: 180000 (3min)
-    settings.condition_1_likes = [12000, 9999999]; // in condition 1, user will receive likes at the following timepoints (in ms), default: [12000, 9999999]
-    settings.condition_2_likes = [10000, 15000,35000,80000,1320000,150000];  // in condition 1, user will receive likes at the following timepoints (in ms), default: [10000, 15000,35000,80000,1320000,150000]
-    settings.condition_3_likes = [10000, 11000,15000,35000,80000,100000,110000,150000,20000];  // in condition 1, user will receive likes at the following timepoints (in ms), default: [10000, 11000,15000,35000,80000,100000,110000,150000,20000]
-    settings.likes_by = ['John','AncaD','NN','Arjen','Jane','George','Dan','Heather','Dan']; // usernames by which the likes will be given, drawn in subsequent order
-    settings.qualtricsid = 'SV_86MZEJccEKhl4qh';
-  }
-
-  // The paradigm consists of a number of slides that participants see one after the other.
-  // The logic of the slides is described in seperate init_... functions.
-  // Usually the slides hide the previous slide, and show the container with the slides content, and define what happens on button clicks, etc.
-
-  // Intro slide with instructions
-  function init_intro() {
-
-    // Show the container with the intro text
-  	$('#intro').show();
 	
-    // Make the intro button clickable
+	// **Number** **of** **Avatar** **Images**   
+	// Number of avatars the user can choose from. Can be changed to any number, depending on how many avatars you would like to display. Default: 82
+	// The avatar images used in the online preview of the paradigm were created using by pickaface.net and due to their terms not available for redistribution. You should therefore create your own images. All images should be 250x250 pixels in size and carry the names "avatar_NUMBER.png" (e.g. avatar_1.png; "png" should be lower case; the numbers in the names should be consequtive, starting from 1). The number of avatars dependeds on the corresponding parameter. The images should be placed in folder "avatars," located in the main study folder extracted on your computer or server.
+
+    settings.numberofavatars = 10;
+
+	
+    // **Redirection**    
+	// After the introduction task is over participants should be redirected to a survey with manipulation checks and dependent measures, to subsequent tasks, or to further instructions. 
+	// If the study is called with a parameter for redirection, as explained in the documentation, this value is overwritten. 
+	// To the redirect link, the following information will be appended: (1) participant number, (2) condition, (3) username, (4) description submitted by participant. These variables can be extracted from the link, saved as data, and used for linking the Social Media Ostracism paradigm to subsequent tasks and measures. See documentation for more details.
+
+    settings.defaultredirect = 'http://fppvu.qualtrics.com/SE/?SID=SV_a9u9MdnpIRuxctT';
+
+	
+	// **Tasklength**     
+    // Length of the group introduction task in milliseconds. Can be changed to any number (in ms). Default: 180000 (3min) 
+    settings.tasklength = 180000; 
+
+	
+	// **Number** **of** **"likes"**    
+    // Each received "like" is indicated by the timepoint (in ms) at which the "like" will appear. To change the number of "likes" in each condition, add or remove timepoints. Make sure that every timepoint (except the first) is preceded by a single comma. 
+	// In cases with only 1 "like," a second "like" is added with time point 9999999. This "like" is added for programming purposes and is never executed, as it is outside the task time
+
+    // In condition 1, the participant will receive 1 like at the following timepoint (in ms). Default: [12000, 9999999]
+    settings.condition_1_likes = [12000, 9999999]; 
+
+    // In condition 2, user will receive 6 likes at the following timepoints (in ms). Default: [10000, 15000,35000,80000,1320000,150000]
+    settings.condition_2_likes = [10000, 15000,35000,80000,1320000,150000];  
+    
+    // In condition 3, user will receive 9 likes at the following timepoints (in ms). Default: [10000, 11000,15000,35000,80000,100000,110000,150000,20000]
+    settings.condition_3_likes = [10000, 11000,15000,35000,80000,100000,110000,150000,20000]; 
+
+	// **Others' likes**     
+	// To keep the total distribution of "likes" constant across conditions, The "likes" received by one group member can be adjusted according to the participant's. By default, the other group member receives 9 "likes" in the participant-ostracism condition, 5 in the participant-inclusion condtion, and 1 in the participant-overinclusion condtion.
+	settings.condition_1_adjusted_likes = [12000, 14000,15000,35000,80000,100000,110000,150000,20000]; // 9
+	settings.condition_2_adjusted_likes = [12000, 14000,15000,35000,80000]; // 5
+	settings.condition_3_adjusted_likes = [12000, 9999999]; //1	
+	
+    // Usernames by which the participant will receive "likes"
+	// If group member names are changed, these should be changed accordingly.
+    settings.likes_by = ['John','AncaD','Sarah','Arjen','Jane','George','Dan','Heather','Ky']; 
+  }
+  
+  // -------------------
+  // Above were the basic parameters you can adjust using the instructions. The remaining code is also annotated, but we do not recommend changing it, unless you are comfortable with web programming.
+  // -------------------
+  
+  
+  // **Slide:** **Intro**     
+  // With instructions regarding the task. The intro container is shown, the continue calls the next slide when clicked.
+  function init_intro() {
+  	$('#intro').show();
   	$('#submit_intro').on('click',function() {
-
-        // Hide the intro slide and show the slide in which participants enter their username
-			  $('#intro').hide();
+			$('#intro').hide();
   			init_name();  			
-
   	});	
   }
   
 
-  // Slide in which participants are asked to enter their username
+  // **Slide:** **Username**       
+  // Note: Only alphanumeric usernames without spaces are accepted
+  
   function init_name() {
 
   	$('#name').show();
 
-    // Only alphanumeric usernames without spaces are accepted
+    
   	$('#submit_username').on('click',function() {
 
   		var error = 0;
@@ -68,13 +106,12 @@ $(function() {
   	});
   }
 
-
+  // **Slide:** **Avatar**       
   // Avatar slide in which the participant is asked to select an avatar
+   
   function init_avatar() {
   	$('#avatar').show();
 
-    // Add avatar images to the slide, avatars are stored in the folder avatars are are numbered as avatar_NUMBER.png
-    // How many avatars are added depends on the according setting
     var avatars = window.settings.numberofavatars;    
   	for(var i=0; i<avatars; i++) 
   	{ 
@@ -100,7 +137,7 @@ $(function() {
   }
 
 
-  // Slide for description
+  // **Slide:** **Description**   
   function init_text() {
   	$('#text').show();
 
@@ -108,7 +145,6 @@ $(function() {
   	  $("#count").text("Characters left: " + (400 - $(this).val().length));
   	});
 
-    // To continue to the next slide, a description between 140 and 400 characters is required
   	$('#submit_text').on('click',function() {
 
   		var error = 0;
@@ -137,7 +173,7 @@ $(function() {
   }
 
 
-  // More instructions
+  // **Slide:** **Instructions**   
   function init_fb_intro() {
   	$('#fb_intro').show();
 	
@@ -150,11 +186,12 @@ $(function() {
   }
 
 
-  // Login screen  
+  // **Slide:** **Login** **Screen**   
+  // Participant can continue after 8000ms = 8s      
   function init_fb_login() {
   	$('#fb_login').show();
 	
-    // Participant can continue after 8000ms = 8s
+
   	setTimeout(function() {
   		$('#msg_all_done').show();
   		$("#loader").hide();
@@ -166,15 +203,13 @@ $(function() {
   	});	
   }
   
-  // Task starts
+  // **Slide:** **Task**   
   function init_task() {
 
     $('#task').show();
 
-    // Unbind backspace key so that the user does not go back to the previous page accidentally
-	  shortcut.add("Backspace",function() {});      
+	shortcut.add("Backspace",function() {});      
 
-    // Init countdown
   	jQuery("#countdown").countDown({
   		startNumber: window.settings.tasklength/1000, // in seconds
   		callBack: function(me) {
@@ -182,9 +217,7 @@ $(function() {
         $('#timer').text('00:00');
   		}
   	});
-	
-    // likes that the user reveive
-
+	   
 		users = {
 		  "posts" : [
 			{
@@ -197,14 +230,14 @@ $(function() {
 		  ]
 		};
 		
-    // Add user box to slide
+    // Add user box to slide     
 	  var tpl = $('#usertmp').html(),html = Mustache.to_html(tpl, users);
 	  $("#task").append(html);
 	  
-    // Add other boxes to slide
+    // Add other boxes to slide    
 	  var tpl = $('#otherstmp').html(),html = Mustache.to_html(tpl, others);
 	  $("#task").append(html);
-
+ 
     // Randomize order of other players boxes
     function reorder() {
        var grp = $("#others").children();
@@ -286,7 +319,7 @@ $(function() {
     $('#final-continue').on('click', function() {
 
       // Redirect link
-      location.href = 'http://fppvu.qualtrics.com/SE/?SID='+settings.qualtricsid+'&p='+window.participant+'&c='+window.condition+'&u='+encodeURI(window.username)+'&av='+window.avatarexport+'&d='+encodeURI(window.description);
+      location.href = 'http://fppvu.qualtrics.com/SE/?SID='+window.qid+'&p='+window.participant+'&c='+window.condition+'&u='+encodeURI(window.username)+'&av='+window.avatarexport+'&d='+encodeURI(window.description);
 
     });
     
@@ -309,13 +342,44 @@ $(function() {
     } else {
       window.participant = 0; // participant defaults to 0
     }    
+    // redirect
+    if(window.QueryString.redirect !== undefined && window.QueryString.redirect !== "") {
+      window.redirect = decode(window.QueryString.redirect);
+    } else {
+	  window.redirect = window.settings.defaultredirect;
+	}
+	
+	var urlHasQuestionMark = (window.redirect.indexOf("?") > -1);
+	if(!urlHasQuestionMark) {
+		window.redirect = window.redirect+"?redir=1";
+	}
+	alert(window.redirect);
 
-    // switch according to condition, set settings.condition_likes
-    window.settings.condition_likes = settings.condition_1_likes;
-
-    // overwrite others according to condition
-    // ...
   }
+  
+  
+  // adjustments according to current condition
+  function adjust_to_condition() {
+
+    // the number of likes a person receives depends on the condition
+	// in addition, the number of likes another person receives is adjusted, so that there is the same number of likes overall
+	switch(condition) {
+		case 1:
+			window.settings.condition_likes = settings.condition_1_likes;
+			window.others.posts[1].likes = settings.condition_1_adjusted_likes;
+			break;
+		case 2:
+			window.settings.condition_likes = settings.condition_2_likes;
+			window.others.posts[1].likes = settings.condition_2_adjusted_likes;
+			break;
+		case 3:
+			window.settings.condition_likes = settings.condition_3_likes;
+			window.others.posts[1].likes = settings.condition_3_adjusted_likes;
+			break;
+	}	
+	  
+  }
+  
 
   // The variable QueryString contains the url parameters, i.e. condition no. and participant no.
   // via http://stackoverflow.com/a/979995
@@ -359,7 +423,16 @@ $(function() {
     return str.length < max ? pad("0" + str, max) : str;
   }
 
+  // Function for encoding and decoding URLs
+  // via http://meyerweb.com/eric/tools/dencoder/
+  function encode(unencoded) {
+	return encodeURIComponent(unencoded).replace(/'/g,"%27").replace(/"/g,"%22");	
+  }
+  function decode(encoded) {
+	return decodeURIComponent(encoded.replace(/\+/g,  " "));
+  }
 
+  
   // Simple Countdown
   // via http://davidwalsh.name/jquery-countdown-plugin
   jQuery.fn.countDown = function(settings,to) {
@@ -400,6 +473,7 @@ $(function() {
   // Set Settings, get Participant No. and Condition No.
   set_settings();
   get_params();
+  adjust_to_condition();
 
   // Start with the intro slide
   init_intro();
